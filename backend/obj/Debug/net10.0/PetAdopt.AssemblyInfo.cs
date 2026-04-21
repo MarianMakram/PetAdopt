907572f643cafb3b23e6395a5c6a67f332ac47e2
MarianMakram <marianmakram303@gmail.com>
2026-04-21 14:00:00 +0200

    update

--- backend/obj/Debug/net10.0/PetAdopt.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/PetAdopt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetAdopt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215f2959234ec1e66ba3fd62c093e171b493ea66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ffe4ffcb4a136e58065f51d20e2e95d85b185f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetAdopt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetAdopt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
